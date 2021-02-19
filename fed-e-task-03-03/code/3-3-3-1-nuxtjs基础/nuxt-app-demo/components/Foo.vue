8c2f5d028c6153d65c6898e2e8884dd87df946b0
shenjianmin <alimincode@163.com>
2021-02-19 17:09:21 +0800

    message

---
+++ fed-e-task-03-03/code/3-3-3-1-nuxtjs基础/nuxt-app-demo/components/Foo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>FooPage</h1>
+    <ul>
+      <li
+        v-for="item in posts"
+        :key="item.id"
+      >
+      <!-- {{ item.title }} -->
+      <nuxt-link :to="'/article/' + item.id">{{ item.title }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooPage',
+  props: ['posts'],
+  // asyncData 只能在页面组件中使用
+  // async asyncData () {
+  //   console.log('foo asyncData')
+  //   return {
+  //     foo: 'bar'
+  //   }
+  // }
+}
+</script>
+
+<style>
+
+</style>
